010fbf15361c46a8f10161d27aa79921e510ee7b
alfesen <89510380+alfesen@users.noreply.github.com>
2023-11-04 21:10:52 +0100

    Create type for Heading

--- client/src/types/typography.ts
+++ client/src/types/typography.ts
@@ -1,3 +1,11 @@
+import type { ReactNode } from 'react'
+
+export type THeading = {
+  semantic: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+  children: ReactNode
+  className?: string
+}
+
 export type TPageTitle = {
   title: string
   subtitle: string
