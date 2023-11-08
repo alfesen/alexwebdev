bf15a83b1dc06bb3cdca201ba9210a0b82bc8c8d
alfesen <89510380+alfesen@users.noreply.github.com>
2023-11-08 19:12:32 +0100

    New types for Slider

--- client/src/types/features.ts
+++ client/src/types/features.ts
@@ -1,8 +1,21 @@
-export type TImage = { src: string; minWidth?: number; maxWidth?: number; alt: string }
+export type TImage = {
+  src: string
+  minWidth?: number
+  maxWidth?: number
+  alt: string
+}
 
 export type TGallery = {
   images: TImage[]
   caption?: string
   className?: string
-  
+}
+
+export type TSlider = { items: Record<string, string>[] }
+
+export type TSliderItem = {
+  offset: number
+  onClick: () => void
+  text: string
+  backgroundImage: string
 }
