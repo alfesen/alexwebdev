c1cf02b7d3bcd0aa94c5b0d7edc538b8cdcef613
alfesen <89510380+alfesen@users.noreply.github.com>
2023-11-16 21:47:08 +0100

    Prevent creating other users

--- server/src/users/auth.service.ts
+++ server/src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { randomBytes, scrypt as _sc } from 'crypto'
 import { promisify } from 'util'
@@ -10,6 +10,10 @@ export class AuthService {
   constructor(private usersService: UsersService) {}
 
   async createUser(email: string, password: string) {
+    const existingUser = await this.usersService.findUser(email)
+    if (existingUser) {
+      throw new BadRequestException('Administrator is already set')
+    }
     const salt = randomBytes(8).toString('hex')
     const hash = (await scrypt(password, salt, 32)) as Buffer
     const result = `${salt}.${hash.toString('hex')}`
