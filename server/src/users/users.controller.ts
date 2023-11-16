e53e2232c9416c9b651b7dc6c295c6684fcdef90
alfesen <89510380+alfesen@users.noreply.github.com>
2023-11-16 21:15:39 +0100

    Create signup controller

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common'
+import { UsersService } from './users.service'
+import { AuthService } from './auth.service'
+import { CreateUserDto } from './dtos/create-user.dto'
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService
+  ) {}
+
+  @Post('signup')
+  async createUser(@Body() { email, password }: CreateUserDto) {
+    const user = await this.authService.createUser(email, password)
+    return user
+  }
+}
