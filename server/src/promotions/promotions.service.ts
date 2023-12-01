28632f58a77685e1108238b77e650379ee994bd1
unknown <alfesen>
2023-12-01 09:29:26 +0100

    Promotion post route

--- server/src/promotions/promotions.service.ts
+++ server/src/promotions/promotions.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Connection, Model } from 'mongoose';
+import { Promotion } from './promotion.schema';
+import { User } from 'src/users/user.schema';
 
 @Injectable()
-export class PromotionsService {}
+export class PromotionsService {
+  constructor(@InjectModel('Promotion') private promotionModel: Model<Promotion>, @InjectModel('User') private userModel: Model<User>, @InjectConnection() private connection: Connection) { }
+
+  async createPromotion(text: string, imageUrl: string, creator: string) {
+    const promotion = new this.promotionModel({
+      text,
+      image: imageUrl,
+      creator
+    })
+
+    await promotion.save()
+
+    const session = await this.connection.startSession()
+    session.startTransaction()
+
+    try {
+      const user = await this.userModel.findById(creator)
+      if (!user) {
+        throw new NotFoundException('User not found')
+      }
+      const promotions = user.promotions
+      promotions.push(promotion.id)
+
+      await user.save()
+      await session.commitTransaction()
+    } catch (error) {
+      await session.abortTransaction()
+      throw new BadRequestException(error.message)
+    } finally {
+      session.endSession()
+    }
+
+    return promotion
+  }
+}
