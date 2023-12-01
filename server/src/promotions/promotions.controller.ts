e62795a107eee5e4276cea16a5684ff9c4f07a0a
unknown <alfesen>
2023-12-01 10:25:13 +0100

    Create Auth Guard + implement in promotions

--- server/src/promotions/promotions.controller.ts
+++ server/src/promotions/promotions.controller.ts
@@ -1,17 +1,15 @@
-import { BadRequestException, Body, Controller, ForbiddenException, NotFoundException, Post, Session } from '@nestjs/common';
+import { Body, Controller, Post, Session, UseGuards } from '@nestjs/common';
 import { PromotionsService } from './promotions.service';
 import { Promotion } from './promotion.schema';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('promotions')
 export class PromotionsController {
   constructor(private readonly promotionsService: PromotionsService) { }
 
+  @UseGuards(AuthGuard)
   @Post()
   async createPromotion(@Body() { text, image }: Promotion, @Session() { userId }: any) {
-    if (!userId) {
-      throw new ForbiddenException('You must be logged in to post the promotion')
-    }
-
     const promotion = await this.promotionsService.createPromotion(text, image, userId)
 
     return promotion
