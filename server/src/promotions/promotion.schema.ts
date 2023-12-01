cd27e7a59a277f5ce918d39bb1ba5ad43d32f310
unknown <alfesen>
2023-12-01 09:06:54 +0100

    Replace Dtos with user and promotion schemas

--- server/src/promotions/promotion.schema.ts
+++ server/src/promotions/promotion.schema.ts
@@ -1,23 +1,24 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import mongoose, { HydratedDocument } from 'mongoose'
-import { User } from 'src/users/user.schema'
+import { IsString, IsMongoId } from 'class-validator'
 
 export type HydratedPromotion = HydratedDocument<Promotion>
 
 @Schema()
 export class Promotion {
+  @IsString()
   @Prop({ required: true })
   text: string
 
+  @IsString()
   @Prop({ required: true })
   image: string
 
+  @IsMongoId()
   @Prop({
-    required: true,
-    type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   })
-  creator: User
+  creator: mongoose.Schema.Types.ObjectId
 }
 
 export const PromotionSchema = SchemaFactory.createForClass(Promotion)
