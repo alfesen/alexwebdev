b6f45f29f9a4ca0e553a953df3058616d0af2f5b
unknown <alfesen>
2023-12-01 09:26:10 +0100

    Replace CreateUserDto with mongoose model

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,20 +1,20 @@
 import { Body, Controller, Get, Post, Session } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './user.schema';
 
 @Controller('users')
 export class UsersController {
   constructor(private authService: AuthService) { }
 
   @Post('signup')
-  async createUser(@Body() { email, password }: CreateUserDto, @Session() session: any) {
+  async createUser(@Body() { email, password }: User, @Session() session: any) {
     const user = await this.authService.createUser(email, password)
     session.userId = user._id
     return user
   }
 
   @Post('signin')
-  async login(@Body() { email, password }: CreateUserDto, @Session() session: any) {
+  async login(@Body() { email, password }: User, @Session() session: any) {
     const user = await this.authService.login(email, password)
     session.userId = user._id
     return this.authService.login(email, password)
