1042cb713da9f6f8551d93fc26a2c9fa7d91d849
unknown <alfesen>
2023-12-18 09:26:22 +0100

    Rename TechStack to TechStackIcons

--- client/src/components/Sections/About/About.tsx
+++ client/src/components/Sections/About/About.tsx
@@ -1,23 +1,23 @@
-import Gallery from '@/components/UI/Gallery/Gallery'
-import s from './About.module.scss'
+import Gallery from "@/components/UI/Gallery/Gallery"
+import s from "./About.module.scss"
+import author from "@/assets/img/author.jpg"
+import { useInView } from "@react-spring/web"
+import TechStackIcons from "../../Features/TechStackIcons/TechStackIcons"
+import AnimatedHeading from "@/components/Typography/AnimatedHeading/AnimatedHeading"
 
-import author from '@/assets/img/author.jpg'
-import { useInView } from '@react-spring/web'
-import TechStack from '../../Features/TechStack/TechStack'
-import AnimatedHeading from '@/components/Typography/AnimatedHeading/AnimatedHeading'
 const About = () => {
   const [ref, inView] = useInView({ once: true })
 
   return (
-    <section ref={ref} className={`${s.about} ${inView ? s.shown : ''}`}>
+    <section ref={ref} className={`${s.about} ${inView ? s.shown : ""}`}>
       <div className={`${s.about__half}`}>
-        <AnimatedHeading semantic='h2' className='u-center' text='Full Stack' />
-        <TechStack />
+        <AnimatedHeading semantic="h2" className="u-center" text="Full Stack" />
+        <TechStackIcons />
       </div>
       <div className={`${s.about__half}`}>
         <Gallery
           className={s.photo}
-          images={[{ src: author, minWidth: 769, alt: 'author' }]}
+          images={[{ src: author, minWidth: 769, alt: "author" }]}
         />
       </div>
     </section>
