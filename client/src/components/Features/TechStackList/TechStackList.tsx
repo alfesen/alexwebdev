80e97a42e3f720a5d07ed645676df742b756e854
unknown <alfesen>
2023-12-20 14:14:46 +0100

    Refactor TechStackList

--- client/src/components/Features/TechStackList/TechStackList.tsx
+++ client/src/components/Features/TechStackList/TechStackList.tsx
@@ -1,12 +1,15 @@
+import { TTechStackList } from "@/types/features"
 import s from "./TechStackList.module.scss"
+import TechStackListItem from "./TechStackListItem"
+import { nanoid } from "nanoid"
 
-const TechStackList = () => {
-  return (
-    <div className={`grid ${s.list}`}>
-      <ul className="grid-content">
-      </ul>
-    </div>
-  )
+const TechStackList = ({ items }: TTechStackList) => {
+  const renderItems = () =>
+    items.map((item) => {
+      return <TechStackListItem key={nanoid()} {...item} />
+    })
+
+  return <ul className={s.list}>{renderItems()}</ul>
 }
 
 export default TechStackList
