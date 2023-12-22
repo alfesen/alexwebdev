fa33cefeea84a631bfb37ae39c3c23119989a51b
unknown <alfesen>
2023-12-22 08:43:44 +0100

    Create animation for individual TechStackListItems

--- client/src/components/Features/TechStackList/TechStackList.tsx
+++ client/src/components/Features/TechStackList/TechStackList.tsx
@@ -1,15 +1,37 @@
 import { TTechStackList } from "@/types/features"
 import s from "./TechStackList.module.scss"
 import TechStackListItem from "./TechStackListItem"
-import { nanoid } from "nanoid"
+import {
+  animated,
+  useChain,
+  useSpringRef,
+  useTransition,
+} from "@react-spring/web"
 
 const TechStackList = ({ items }: TTechStackList) => {
-  const renderItems = () =>
-    items.map((item) => {
-      return <TechStackListItem key={nanoid()} {...item} />
-    })
+  const transRef = useSpringRef()
+  const transitions = useTransition(items, {
+    ref: transRef,
+    trail: 1300 / items.length,
+    reset: !!items,
+    from: { opacity: 0, transform: "translate3d(-50px, 0, 0)" },
+    enter: { opacity: 1, transform: "translate3d(0, 0, 0)" },
+    leave: { opacity: 1, transform: "translate3d(0, 0, 0)" },
+  })
 
-  return <ul className={s.list}>{renderItems()}</ul>
+  useChain([transRef], [0])
+
+  return (
+    <ul className={s.list}>
+      {transitions((style, item) => {
+        return (
+          <animated.div style={style}>
+            <TechStackListItem {...item} />
+          </animated.div>
+        )
+      })}
+    </ul>
+  )
 }
 
 export default TechStackList
