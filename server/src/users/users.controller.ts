2ff00f497efdf4ff76699b90097364133c03c511
unknown <alfesen>
2024-01-05 15:29:23 +0100

    Apply new cookie logic

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,25 +1,32 @@
-import { Body, Controller, Post, Session } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { User } from './user.schema';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
-import { UserDto } from './dtos/user.dto';
+import { Body, Controller, Post, Res } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { User } from './user.schema'
+import { Serialize } from 'src/interceptors/serialize.interceptor'
+import { UserDto } from './dtos/user.dto'
+import { Response } from 'express'
+
+const expirationHours = 1
+const expirationDate = new Date()
+expirationDate.setTime(
+  expirationDate.getTime() + expirationHours * 60 * 60 * 1000
+)
 
 @Controller('users')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
   @Post('signup')
-  async createUser(@Body() { email, password }: User, @Session() session: any) {
+  async createUser(@Body() { email, password }: User, @Res() res: Response) {
     const user = await this.authService.createUser(email, password)
-    session.userId = user._id
+    res.cookie('isAuth', user._id.toString(), { expires: expirationDate })
     return user
   }
 
   @Post('signin')
-  async login(@Body() { email, password }: User, @Session() session: any) {
+  async login(@Body() { email, password }: User, @Res() res: Response) {
     const user = await this.authService.login(email, password)
-    session.userId = user._id
-    return this.authService.login(email, password)
+    res.cookie('isAuth', user._id.toString(), { expires: expirationDate })
+    return user
   }
 }
