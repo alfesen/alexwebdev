d404b11c915742e7770f661ba7f2536fb5e48f83
unknown <alfesen>
2024-01-18 09:02:52 +0100

    Logic for promotion update and deleting

--- server/src/promotions/promotions.controller.ts
+++ server/src/promotions/promotions.controller.ts
@@ -1,43 +1,70 @@
 import {
   Body,
   Controller,
+  Delete,
+  Param,
+  Patch,
   Post,
   Req,
   UseGuards,
-} from '@nestjs/common'
-import { PromotionsService } from './promotions.service'
-import { AuthGuard } from 'src/guards/auth.guard'
-import { UploadImage } from 'src/decorators/upload-image.decorator'
-import { Request } from 'express'
-import { PromotionDto } from './dtos/promotion.dto'
-import { SharpImage } from 'src/decorators/sharp-image.decorator'
+} from "@nestjs/common"
+import { PromotionsService } from "./promotions.service"
+import { AuthGuard } from "src/guards/auth.guard"
+import { UploadImage } from "src/decorators/upload-image.decorator"
+import { Request } from "express"
+import { PromotionDto } from "./dtos/promotion.dto"
+import { SharpImage } from "src/decorators/sharp-image.decorator"
 
-@Controller('promotions')
+@Controller("promotions")
 export class PromotionsController {
   constructor(private readonly promotionsService: PromotionsService) {}
 
   @UseGuards(AuthGuard)
   @Post()
-  @UploadImage('image')
+  @UploadImage("image")
   async createPromotion(
     @Body() { text }: PromotionDto,
     @Req() request: Request,
-    @SharpImage(1200, true) image: string
+    @SharpImage(1200, true) image: string,
   ) {
     const cookies = request.headers.cookie
-    const cookiesArray: string[] = cookies ? cookies.split('; ') : []
+    const cookiesArray: string[] = cookies ? cookies.split("; ") : []
     const isAuthCookie = cookiesArray.find((cookie) =>
-      cookie.startsWith('isAuth=')
+      cookie.startsWith("isAuth="),
     )
 
-    const userId = isAuthCookie.split('=')[1]
+    const userId = isAuthCookie.split("=")[1]
 
     const promotion = await this.promotionsService.createPromotion(
       text,
       image,
-      userId
+      userId,
     )
 
     return promotion
+  }
+
+  @UseGuards(AuthGuard)
+  @Patch("/:id")
+  @UploadImage("image")
+  async updatePromotion(
+    @Param("id") id: string,
+    @Body() { text }: PromotionDto,
+    @SharpImage(1200, false) image: string,
+  ) {
+    const newTech = await this.promotionsService.updateTech(
+      id,
+      text,
+      image ? image : undefined,
+    )
+
+    return newTech
+  }
+
+  @UseGuards(AuthGuard)
+  @Delete("/:id")
+  async deletePromotion(@Param("id") id: string) {
+    await this.promotionsService.deletePromotion(id)
+    return { message: "Promotion successfully deleted" }
   }
 }
