92730a00aab29ff6733e5ffb301ec118ce738204
unknown <alfesen>
2024-01-31 09:54:59 +0100

    Install multer for build purposes

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ import * as session from 'express-session'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: [process.env.ADMIN_CLIENT, process.env.CLIENT],
+    origin: [process.env.ADMIN_CLIENT, process.env.CLIENT, process.env.PROD_CLIENT],
     credentials: true
   })
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
