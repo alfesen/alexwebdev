97b6eb8a45cf6788ff41b485b232025acdedd11b
unknown <alfesen>
2024-02-01 09:29:32 +0100

    Prod

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -26,7 +26,8 @@ export class UsersController {
   @Post('signin')
   async login(@Body() { email, password }: User, @Res() res: Response) {
     const user = await this.authService.login(email, password)
-    res.cookie('isAuth', `${user._id.toString()}; Partitioned`, { expires: expirationDate })
+    res.cookie('isAuth', user._id.toString(), { expires: expirationDate })
+    res.cookie('Partitioned', true, { expires: expirationDate })
     return res
   }
 }
