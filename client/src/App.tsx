4ea82f384e4d41648eccc062cd087f3134fc60bd
unknown <alfesen>
2024-02-05 13:59:07 +0100

    Implement emailjs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
-import MainLayout from "./components/Layout/MainLayout";
-import About from "./components/Sections/About/About";
-import Contact from "./components/Sections/Contact/Contact";
-import Promotion from "./components/Sections/Promotion/Promotion";
-import TechStack from "./components/Sections/TechStack/TechStack";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { useEffect } from 'react'
+import MainLayout from './components/Layout/MainLayout'
+import About from './components/Sections/About/About'
+import Contact from './components/Sections/Contact/Contact'
+import Promotion from './components/Sections/Promotion/Promotion'
+import TechStack from './components/Sections/TechStack/TechStack'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import emailjs from '@emailjs/browser'
 
 function App() {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient()
+
+  useEffect(() => {
+    emailjs.init(process.env.EMAIL_PUBLIC_KEY as string)
+  }, [])
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -17,7 +23,7 @@ function App() {
         <Contact />
       </MainLayout>
     </QueryClientProvider>
-  );
+  )
 }
 
-export default App;
+export default App
